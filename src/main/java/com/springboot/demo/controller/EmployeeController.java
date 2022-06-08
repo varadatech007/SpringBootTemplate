6302dd6954e3ce9e7781ac5ab31920f17bf47669
Nivarti Srinivas <srinivas.nvarti.partner@decathlon.com>
2022-06-08 21:17:58 +0530

    modalchange

--- src/main/java/com/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/springboot/demo/controller/EmployeeController.java
@@ -17,8 +17,52 @@ public class EmployeeController {
 		em.setFirstName("Varada");
 		em.setLastName("Ramana");
 		em.setDepartment("IT");
-		em.setSalary(5000);
+		em.setSalary(50000);
 		return em;
+	
+		}
+	@GetMapping("/empdata1")
+	public EmployeeModel getEmployee1() {
+		EmployeeModel em1 = new EmployeeModel();
+		em1.setFirstName("Talari");
+		em1.setLastName("Sankar");
+		em1.setDepartment("IT");
+		em1.setSalary(40000);
+		return em1;
+	
+		}
+	
+	@GetMapping("/empdata2")
+	public EmployeeModel getEmployee2() {
+		EmployeeModel em2 = new EmployeeModel();
+		em2.setFirstName("Kiran");
+		em2.setLastName("Kumar");
+		em2.setDepartment("IT");
+		em2.setSalary(40000);
+		return em2;
+	
+		}
+	@GetMapping("/empdata3")
+	public EmployeeModel getEmployee3() {
+		EmployeeModel em3 = new EmployeeModel();
+		em3.setFirstName("Sateesh");
+		em3.setLastName("Kumar");
+		em3.setDepartment("IT");
+		em3.setSalary(40000);
+		return em3;
+	
+		}
+	
+	@GetMapping ("/empdata4")
+	public EmployeeModel getEmployee4(){
+		EmployeeModel em4 = new EmployeeModel();
+		em4.setFirstName("Talari");
+		em4.setLastName("Venkatesh");
+		em4.setDepartment("IT");
+		em4.setSalary(40000);
+		return em4;
+	
+	}
 	}
 
-}
+
