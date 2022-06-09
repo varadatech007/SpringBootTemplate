b213e40a60a33f72bdd8c72d676e152a43670105
sateesh_june <sateesh.patakamsetty@gmail.com>
2022-06-09 15:16:46 +0530

    Modified Controller

--- src/main/java/com/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/springboot/demo/controller/EmployeeController.java
@@ -19,6 +19,15 @@ public class EmployeeController {
 		em.setDepartment("IT");
 		em.setSalary(5000);
 		return em;
+		
+		}
+	@GetMapping("/empdata1")
+	public EmployeeModel getEmployee1() {
+		EmployeeModel em1 = new EmployeeModel();
+		em1.setFirstName("Sateesh");
+		em1.setLastName("Patakamsetty");
+		em1.setDepartment("IT");
+		em1.setSalary(2000);
+		return em1;
 	}
-
 }
