3ce180a5ddd17b806024e3da4ed1ac610d3fb2bf
55153456 <kirankumarkancharl95@gmail.com>
2022-06-09 21:06:54 +0530

    mobiletypes

--- src/main/java/com/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/springboot/demo/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.demo.model.EmployeeModel;
+import com.springboot.demo.model.MobileModels;
 
 @RestController
 @RequestMapping("/v1/api")
@@ -19,6 +20,20 @@ public class EmployeeController {
 		em.setDepartment("IT");
 		em.setSalary(5000);
 		return em;
+	}
+	
+	@GetMapping("/mobdata")
+	public MobileModels getMobileModels() {
+		
+		MobileModels mb = new MobileModels();
+		mb.setMobName("Redmi");
+		mb.setMobSeries("Redmi10s");
+		mb.setMobPrize(25000);
+		mb.setMobImei(98883899);
+		mb.setMobOsType("Android");
+		
+		return mb;
+		
 	}
 
 }
---
+++ src/main/java/com/springboot/demo/model/MobileModels.java
@@ -0,0 +1,18 @@
+package com.springboot.demo.model;
+
+import lombok.Data;
+
+@Data
+
+public class MobileModels {
+	
+   private String mobName;
+   private String mobSeries;
+   private Integer mobPrize;
+   private Integer mobImei;
+   private String mobOsType;
+   
+   
+   
+
+}
