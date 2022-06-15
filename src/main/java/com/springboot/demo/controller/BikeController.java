ad12a096699afccb5f77ad1e8d558a36c6a3c3f8
jagadish <jagadishsai555@gmail.com>
2022-06-15 19:53:31 +0530

    vindula

--- src/main/java/com/springboot/demo/controller/BikeController.java
+++ src/main/java/com/springboot/demo/controller/BikeController.java
@@ -1,5 +1,7 @@
 package com.springboot.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +24,14 @@ public class BikeController {
 		return bs;
 		
 	}
+	@GetMapping("/sai")
+	public List<BikeModel>  getBikeModels(){
+		List<BikeModel> bikemodels = bikeService.getBikeModels();
+		return bikemodels;
+		
 	
 	
 
 	
+}
 }
--- src/main/java/com/springboot/demo/service/BikeService.java
+++ src/main/java/com/springboot/demo/service/BikeService.java
@@ -1,5 +1,8 @@
 package com.springboot.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.springboot.demo.model.BikeModel;
@@ -15,10 +18,42 @@ public class BikeService {
 		bm.setBikeweight("200");
 		bm.setBikeyear("2019");
 		return bm;
-		
-		
-		
-		
+			
 	}
 
+	public List<BikeModel>  getBikeModels(){
+		List<BikeModel> sai = new ArrayList<>();
+		
+		BikeModel bm = new BikeModel();
+		bm.setBikecompany("tvs");
+		bm.setBikemodel("apache");
+		bm.setBikecc("159");
+		bm.setBikeweight("200");
+		bm.setBikeyear("2019");
+		sai.add(bm);
+		
+		
+		
+		BikeModel b = new BikeModel();
+		b.setBikecompany("hero");
+		b.setBikemodel("passion");
+		b.setBikecc("125");
+		b.setBikeweight("180");
+		b.setBikeyear("2022");
+	    sai.add(b);
+		
+		
+		
+		BikeModel a = new BikeModel();
+		a.setBikecompany("honda");
+		a.setBikemodel("dio");
+		a.setBikecc("125");
+		a.setBikeweight("180");
+		a.setBikeyear("2020");
+		sai.add(a);
+		
+		
+		return sai;
+	
+	}
 }
