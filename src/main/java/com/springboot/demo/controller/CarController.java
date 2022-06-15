b947e9cb4026a6c1e61c797a214964b3d22a7ef1
55153456 <kirankumarkancharl95@gmail.com>
2022-06-15 20:54:30 +0530

    Array list added

--- src/main/java/com/springboot/demo/controller/CarController.java
+++ src/main/java/com/springboot/demo/controller/CarController.java
@@ -1,40 +1,62 @@
 package com.springboot.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.springboot.demo.model.CarModel;
 import com.springboot.demo.service.CarService;
 
 @RestController
 @RequestMapping("/car/api")
 public class CarController {
-	
+
 	@Autowired
 	CarService getcarService;
-	
-	@GetMapping("/crd")
+
+	@GetMapping("/crj")
 	public CarModel getCarModel() {
 		CarModel cm = getcarService.getCarModel();
 		return cm;
-		
-	
 	}
-	
+
 	@GetMapping("/crk")
 	public CarModel getCarModel2() {
 		CarModel cm1 = getcarService.getCarModel2();
 		return cm1;
 	}
-	
-	@GetMapping("/crm")
+
+	@GetMapping("/crl")
 	public String getCarModel3() {
 		CarModel cm2 = getcarService.getCarModel();
 		String cnm = cm2.getCarName();
 		return cnm;
-		
-	
+
 	}
+
+	@GetMapping("/crm")
+	public List<CarModel> getCarModel4() {
+		List<CarModel> list = getcarService.getCarModels();
+		return list;
+	}
+
+	@GetMapping("/crn")
+	public List<CarModel> getCarModel5() {
+		List<CarModel> list2 = getcarService.getCarModels2();
+		return list2;
+	}
+
+	@GetMapping("/cro")
+	public List<CarModel> getCarModels6() {
+		List<CarModel> list3 = getcarService.getCarModels3();
+		return list3;
+	}
+
 }
--- src/main/java/com/springboot/demo/service/CarService.java
+++ src/main/java/com/springboot/demo/service/CarService.java
@@ -1,5 +1,9 @@
 package com.springboot.demo.service;
 
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.springboot.demo.model.CarModel;
@@ -7,24 +11,62 @@ import com.springboot.demo.model.CarModel;
 @Service
 public class CarService {
 	public CarModel getCarModel() {
-		CarModel cr = new CarModel();
-		cr.setCarName("Benz");
-		cr.setCarModel("Mercidows");
-		cr.setCarType("Petrol");
-		cr.setCarSeries("High Class");
-		cr.setCarLocation("Hyderabad");
-		return cr;
-		
+		CarModel crk = new CarModel();
+		crk.setCarName("Benz");
+		crk.setCarModel("Mercidows");
+		crk.setCarType("Petrol");
+		crk.setCarSeries("High Class");
+		crk.setCarLocation("Hyderabad");
+		return crk;
+
 	}
 
 	public CarModel getCarModel2() {
+		CarModel crl = new CarModel();
+		crl.setCarName("Audi");
+		crl.setCarModel("a48");
+		crl.setCarType("disel");
+		crl.setCarSeries("Low Class");
+		crl.setCarLocation("Chennai");
+		return crl;
+
+	}
+
+	public List<CarModel> getCarModels() {
+		List<CarModel> list = new ArrayList<CarModel>();
 		CarModel crm = new CarModel();
-		crm.setCarName("Audi");
-		crm.setCarModel("a48");
-		crm.setCarType("disel");
-		crm.setCarSeries("Low Class");
-		crm.setCarLocation("Chennai");
-		return crm;
-		
+		crm.setCarName("Range Rover");
+		crm.setCarModel("2018");
+		crm.setCarType("Petrol");
+		crm.setCarSeries("Sport");
+		crm.setCarLocation("Bangalore");
+		list.add(crm);
+		return list;
+	}
+
+	public List<CarModel> getCarModels2() {
+		List<CarModel> list2 = new ArrayList<CarModel>();
+		CarModel crn = new CarModel();
+		crn.setCarName("Land Rover");
+		crn.setCarModel("2020");
+		crn.setCarType("Petrol");
+		crn.setCarSeries("Speed");
+		crn.setCarLocation("Mumbai");
+		list2.add(crn);
+
+		return list2;
+
+	}
+
+	public List<CarModel> getCarModels3() {
+		List<CarModel> list3 = new ArrayList<CarModel>();
+		CarModel cro = new CarModel();
+		cro.setCarName("Ferrari");
+		cro.setCarModel("2021");
+		cro.setCarType("Petrol");
+		cro.setCarSeries("Sports");
+		cro.setCarLocation("Delhi");
+		list3.add(cro);
+		return list3;
 	}
 }
